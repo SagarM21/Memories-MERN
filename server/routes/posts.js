18fa02c537292ec655aa518508a9523090ef22f9
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2021-07-14 19:08:51 +0530

    Fetch post backend complete

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,13 @@
 import express from 'express';
 
-import { getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost } from '../controller/posts.js';
+import { getPost, getPostsBySearch, getPosts, createPost, updatePost, deletePost, likePost } from '../controller/posts.js';
 import auth from '../middleware/auth.js'
 
 const router = express();
 
 router.get('/' , getPosts);
 router.get('/search', getPostsBySearch)
+router.get('/:id', getPost)
 router.post('/' , auth, createPost);
 router.patch('/:id', auth, updatePost);     //patch is used to update.
 router.delete('/:id', auth, deletePost);
